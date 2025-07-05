c30c898af4f49d30e4039db783d0a1137d2045ea
abhishek9sharma <abhishek9sharma@users.noreply.github.com>
2025-07-05 18:43:03 +0800

    add error handling

--- src/streamlit/pages/guarded.py
+++ src/streamlit/pages/guarded.py
@@ -45,29 +45,34 @@ def guarded_chat():
             st.markdown(prompt)
 
         with st.chat_message("assistant"):
-            response = requests.post(
-                "http://localhost:8000/ChatCompletionsReqGuarded",
-                headers={
-                    "accept": "application/json",
-                    "Content-Type": "application/json",
-                },
-                json={
-                    "messages": st.session_state.messages,
-                    "stream": True,
-                    "max_tokens": 50,
-                    "model": "gpt-3.5-turbo",
-                    "input_guard_to_apply": "Profanity",
-                    "output_guard_to_apply": "PII",
-                },
-            )
-            if response.status_code == 200:
-                content = response.text
-                st.markdown(content)
-                st.session_state.messages.append(
-                    {"role": "assistant", "content": content}
+            try:
+                response = requests.post(
+                    "http://localhost:8000/ChatCompletionsReqGuarded",
+                    headers={
+                        "accept": "application/json",
+                        "Content-Type": "application/json",
+                    },
+                    json={
+                        "messages": st.session_state.messages,
+                        "stream": True,
+                        "max_tokens": 50,
+                        "model": "gpt-3.5-turbo",
+                        "input_guard_to_apply": "Profanity",
+                        "output_guard_to_apply": "PII",
+                    },
                 )
-            else:
-                st.error(f"Error: {response.text}")
+                if response.status_code == 200:
+                    content = response.text
+                    st.markdown(content)
+                    st.session_state.messages.append(
+                        {"role": "assistant", "content": content}
+                    )
+                else:
+                    st.error(f"Error: {response.text}\n\n Please make sure you have run the notebook 02_serving_gr_fastapi.ipynb to start the FastAPI server.")
+                    st.markdown("Please make sure you have run the notebook 02_serving_gr_fastapi.ipynb to start the FastAPI server")
+            except requests.exceptions.RequestException as e:
+                st.error(f"Request failed: {e}")
+                st.markdown("Please make sure you have run the full notebook 02_serving_gr_fastapi.ipynb to start the FastAPI server")
 
 
 if __name__ == "__main__":
